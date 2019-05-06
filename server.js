84ecd85bad721da8e0712cc41f80ab47d135dde4
leo <leonelvinasco@gmail.com>
2019-05-06 01:49:54 -0500

    Servidor y direcciones básicas

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express=require('express');
 var app = express();
 
-app.get('/',(request,response)=>{
-  
+app.get('/',(req,res)=>{
+  //res.send('<h1>Hello Express</h1>');
+  res.send({
+    name:'leo',
+    gustos:[
+      'ciclismo',
+      'helados'
+    ]
+  });
 });
+
+app.get('/about',(req,res)=>{
+  res.send('<h1>about</h1>');
+});
+
+app.get('/bad',(req,res)=>{
+  res.send({
+    errorMessage: '404 not found',
+    statusCode: 'Failed'
+  });
+});
+
+app.listen(3000);
