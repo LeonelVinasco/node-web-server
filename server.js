cd523449b85cbc4d6ad75bde9aebf51fedaff096
leo <leonelvinasco@gmail.com>
2019-05-06 15:13:32 -0500

    enviando JSON data al template

--- server.js
+++ server.js
@@ -2,25 +2,37 @@ const express=require('express');
 const hbs =require('hbs');
 var app = express();
 
-app.use(express.static(__dirname+'/public'));
-
 app.set('view engine','hbs');
 
+app.use(express.static(__dirname+'/public'));
+var data= {
+  name:'leo',
+  gustos:[
+    'ciclismo',
+    'helados'
+  ]
+}
+
+var datos=JSON.stringify(data);
 app.get('/',(req,res)=>{
 
-
-  res.send('<h1>Hello Express</h1>');
-  res.send({
-    name:'leo',
-    gustos:[
-      'ciclismo',
-      'helados'
-    ]
+  res.render('home.hbs', {
+    pageTitle:'Welcome to my site',
+    currentYear: new Date().getFullYear(),
+    datos,
+    data
   });
+
+  //res.send('<h1>Hello Express</h1>');
+  //res.send();
 });
 
 app.get('/about',(req,res)=>{
-  res.send('<h1>about, page</h1>');
+  res.render('about.hbs', {
+    pageTitle:'Leo`s About Page',
+    currentYear: new Date().getFullYear()
+
+  });
 });
 
 app.get('/bad',(req,res)=>{
